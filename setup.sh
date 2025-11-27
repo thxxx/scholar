846d0748569912e6082c20d6a64392aa2b697109
thxxx <zxcv05999@naver.com>
2025-11-27 19:29:34 +0900

    save

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 sudo apt-get update
 sudo apt-get install -y ffmpeg
 sudo apt-get install -y python3-pip unzip wget
-sudo apt-get install xvfb google-chrome-stable
-pip install tqdm pandas selenium requests selenium-stealth beautifulsoup4
+sudo apt-get install -y xvfb google-chrome-stable
+
+git config user.email zxcv05999@naver.com
+git config user.name thxxx
+
+python3 -m pip install tqdm pandas selenium requests selenium-stealth beautifulsoup4
 # pip install tqdm pandas selenium requests google-cloud-storage blinker==1.4.0 selenium-wire --break-system-packages
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
+sudo apt-get install -f -y
 # sudo apt-get -f install -y
 # CHROME_DRIVER_VERSION=$(wget -q -O - https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 # wget https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
