b0c653114b3d7e5bcf1bc9ea7495fc2975ac8441
Kim Hojin <zxcv05999@naver.com>
2025-11-27 17:23:53 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install -y ffmpeg
 sudo apt-get install -y python3-pip unzip wget
 sudo apt-get install xvfb google-chrome-stable
-pip install tqdm pandas selenium requests selenium-stealth
+pip install tqdm pandas selenium requests selenium-stealth beautifulsoup4
 # pip install tqdm pandas selenium requests google-cloud-storage blinker==1.4.0 selenium-wire --break-system-packages
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
