8c156de8ed87aefdda5539529cba1b4f2849c326
thxxx <zxcv05999@naver.com>
2025-12-01 03:50:42 +0000

    test

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y ffmpeg
+sudo apt-get install -y ffmpeg, vim
 sudo apt-get install -y python3-pip unzip wget
 sudo apt-get install -y xvfb google-chrome-stable
 
